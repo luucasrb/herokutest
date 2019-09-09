7aa821cc920cc86419a39b974d8aa139a9983216
Lucas Ribeiro <luucas-rb@hotmail.com>
2019-09-09 19:33:56 -0300

    Email Update

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Criador"] = "Lucas Hokage"
+	c.Data["Email"] = "luucas-rb@hotmail.com"
 	c.TplName = "index.tpl"
 }
