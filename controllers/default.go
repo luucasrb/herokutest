e340a1261930adf067e196aa2da1ab1fb0e45614
Lucas Ribeiro <luucas-rb@hotmail.com>
2019-09-09 19:54:49 -0300

    Index

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "github.com/luucasrb"
-	c.Data["Email"] = "luucas-rb@hotmail.com"
-	c.TplName = "index.tpl"
+	c.Data["Website"] = "github.com/saulopinedo/xiaomi-chat"
+	c.Data["Email"] = "lucasribeiro@furg.br"
+	c.TplName = "index.html"
 }
